6fa090fac070dc4a181315927ab8bbe33a690786
Anderson Lopes <anderson.lopes.2010@gmail.com>
2025-04-29 21:49:40 +0200

    feat: Added internationalization

--- src/main/java/com/lopes/workhours/WorkHoursApplication.java
+++ src/main/java/com/lopes/workhours/WorkHoursApplication.java
@@ -9,8 +9,7 @@ public class WorkHoursApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkHoursApplication.class, args);
     }
-
-    // TODO Add internationalization (EN, PT, FR, NL)
+    
     // TODO Add charts filters
     // TODO Review charts - Create native queries
 
